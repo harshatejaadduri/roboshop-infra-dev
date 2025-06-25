0ea35f04ba0a0af9f929938e7e8ad6bfda3067fa
harshatejaadduri <harshateja3@gmail.com>
2025-06-25 23:55:16 +0530

    Updated

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -1,7 +1,7 @@
 module "vpc" {
   source = "git::https://github.com/harshatejaadduri/terraform-vpc.git?ref=main"
-  project = "roboshop"
-  environment = "dev"
+  project = var.project
+  environment = var.environment
   public_cidr_block  = var.public_subnet
   private_cidr_block = var.private_subnet
   database_cidr_block = var.database_subnet
