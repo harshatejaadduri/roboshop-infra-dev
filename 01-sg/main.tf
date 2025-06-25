00eb376b193d4e2f351a856acdcf7e81a820696c
harshatejaadduri <harshateja3@gmail.com>
2025-06-25 23:42:22 +0530

    Updated

---
+++ 01-sg/main.tf
@@ -0,0 +1,9 @@
+module "frontend_sg" {
+  source = "../../terraform-sg"
+  project = var.project
+  environment = var.environment
+
+  sg_name = var.frontend_sg_name
+  sg_description = var.frontend_sg_description
+  vpc_id = local.vpc_id
+}
