899a1677015af4de35fa0f1c80496a1d0b116f7b
harshatejaadduri <harshateja3@gmail.com>
2025-06-26 21:39:24 +0530

    Updated

--- 10-sg/locals.tf
+++ 10-sg/locals.tf
@@ -1,3 +1,4 @@
 locals {
     vpc_id = data.aws_ssm_parameter.vpc_id.value
+    bastion_sg_id = data.aws_ssm_parameter.bastion_sg_id.value
 }
--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -7,3 +7,22 @@ module "frontend_sg" {
   sg_description = var.frontend_sg_description
   vpc_id = local.vpc_id
 }
+
+module "bastion_sg" {
+  source = "git::https://github.com/harshatejaadduri/terraform-sg.git?ref=main"
+  project = var.project
+  environment = var.environment
+
+  sg_name = var.bastion_sg_name
+  sg_description = var.bastion_sg_description
+  vpc_id = local.vpc_id
+}
+
+resource "aws_security_group_rule" "bastion_local" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.bastion_sg.sg_id
+}
